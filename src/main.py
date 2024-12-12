8937bf0b49501132f349be60a62ed0b396f487fa
Lucas Bueno Raubach <lucasbraubach@gmail.com>
2024-12-11 21:40:50 -0300

    aaa

--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
 import pyxel
 
 class HitBox:
-    def __init__(self, name:str, id:int, element:type) -> None:
-        self.name:str = name
+    def __init__(self, element:type, id:int, x:int, y:int, h:int, w:int) -> None:
         self.type:str = element.__name__
         self.id:int = id
-        self.poits:list[list] = [[0,0],[0,0]]
-
+        self.x:int = x
+        self.y:int = y
+        self.x1:int = 0
+        self.y1:int = 0
+        self.heigth:int = h
+        self.width:int = w
+    
+    def update(self, x:int, y:int):
+        self.x = x
+        self.y = y; 
+        self.x1 = self.x + self.width
+        self.y1 = self.y + self.heigth
 
 class Collision:
     def __init__(self) -> None:
-        self.listObjects:list[HitBox] = []
+        self.list:list[HitBox] = []
     
-    def test(self) -> None:
-        pass        
+    def test(self) -> list[HitBox]:
+        pass
 
 class Sprite:
     def __init__(self, x:float, y:float, velocity:float) -> None:
@@ -21,10 +30,10 @@ class Sprite:
         self.y:float = y
         self.velocity:float = velocity
         
-    def draw(self, frameCout):
+    def draw(self, frameCount:int):
         pass
 
-    def update(self, ):
+    def update(self, frameCount:int):
         pass
 
     def walk_up(self):
@@ -46,14 +55,15 @@ class Player(Sprite):
         self.sprite:int = 6
         
         self.inCooldown:bool = False
-        self.colldownTime:float = 30 * 1
+        self.colldownTime:float = 30 * 0.5
         self.lastShotFrame:int = 0
     
     def shot(self, frameCount:int):
         self.lastShotFrame = frameCount
+        self.inCooldown = True
 
     def __cooldownShot(self, frameCount:int):
-        if frameCount >= self.lastShotFrame + self.colldown:
+        if (frameCount >= self.lastShotFrame + self.colldownTime) and self.inCooldown:
             self.inCooldown = False
 
     def update(self, frameCount:int):
@@ -65,18 +75,24 @@ class Player(Sprite):
 
 
 class Shot:
-    def __init__(self, x:float,y:float, velocity:int, player:bool) -> None:
+    def __init__(self, x:float,y:float, velocity:int, id:int ,player:bool) -> None:
         self.x:float = x
         self.y:float = y
+        self.id:int  = id
         self.player:bool = player
         self.velocity:int = velocity
+        self.hitbox = HitBox(type(self), x ,y ,id , 8, 2)
         self.index_image:tuple = (8, 0)
     
     def update(self) -> None:
         self.y += self.velocity
+        self.hitbox.update(self.x, self.y)
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, 16 * self.index_image[0], 16 * self.index_image[1], 16,16)
+        pyxel.blt(self.x, self.y, 0, 16 * self.index_image[0] + 7, 16 * self.index_image[1] + 4, 2,8)
+    
+    def getHitbox(self):
+        return self.hitbox
 
 
 class ShotList:
@@ -84,7 +100,7 @@ class ShotList:
         self.shotList:list[Shot] = []
 
     def shot(self, x:float, y:float, velocity:int, player:bool):
-        self.shotList.append(Shot(x,y,velocity,player))
+        self.shotList.append(Shot(x,y,velocity,player, len(self.shotList)))
 
     def update(self):
         for i in range(len(self.shotList)):
@@ -112,7 +128,7 @@ class App:
 
         self.load_imagens()
 
-        # self.collision:Collision = Collision()
+        self.collision:Collision = Collision()
         self.player:Player = Player()
         self.shotList:ShotList = ShotList()
 
@@ -124,18 +140,22 @@ class App:
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
 
+        # Colisions
         # Update class
         self.player.update(self.frameCout)
 
         # Keys
         if pyxel.btn(pyxel.KEY_LEFT): self.player.walk_left()
         if pyxel.btn(pyxel.KEY_RIGHT): self.player.walk_rigth()
-        if pyxel.btnp(pyxel.KEY_SPACE): self.shotList.shot(self.player.x, self.player.y, -4, True)
+        
+        if pyxel.btnp(pyxel.KEY_SPACE) and not self.player.inCooldown: 
+            self.shotList.shot(self.player.x+7, self.player.y, -4, True)
+            self.player.shot(self.frameCout)
 
         self.shotList.update()
        
     def draw(self):
-        pyxel.cls(7)
+        pyxel.cls(0)
         self.shotList.draw()
         self.player.draw()
 
